8327875a5918ee991f702206c1d52dd599cf890a
Unknown <a.searle274@gmail.com>
2017-10-10 06:55:58 -0500

    done loop after shift

--- sch17f__comp3350__hw3/main.asm
+++ sch17f__comp3350__hw3/main.asm
@@ -8,21 +8,34 @@
 ExitProcess proto,dwExitCode:dword
 
 .data
-	input byte 1,2,3
+	input byte 4,06h,8,0Ch
 	output byte lengthof input DUP(?)
-	shift byte 3
+	shift dword 2
 
 .code
 	main proc
-		mov esi, 0
-		mov ecx, lengthof input 
+		mov esi, 0							; current index
+		mov ebx, shift						; index + shift
+		mov ecx, lengthof input				; counter == input[]
 		L:
-			mov al, input[esi]			; get a character from source
-			mov output[esi], al			; store it in the target
-			inc esi
-
+			mov al, input[esi]				; get a character from source
+			mov output[ebx], al				; store it in the target
+			inc esi							; move to next character in input[]
+			inc ebx							; move to next character in output[]
 		loop L
 		
+		
+		
+		
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		; loop for testing
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		mov esi, 0
+		mov ecx, lengthof input
+		A:
+			mov al, output[esi]				; get a character from source
+			inc esi							; move to next character
+		loop A
 		
 		invoke ExitProcess, 0
 	main endp
