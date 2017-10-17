589340e7cf6da731d57517f9c34e84eb33548d52
Ash <a.searle274@gmail.com>
2017-10-16 20:46:04 -0500

    done, switched loops

--- sch17f__comp3350__hw3/main.asm
+++ sch17f__comp3350__hw3/main.asm
@@ -8,24 +8,13 @@
 ExitProcess proto,dwExitCode:dword
 
 .data
-	input byte  5,0Ah,3,6,0Ch
+	input byte  1,2,3,4,5,6,7,8,9,0Ah
 	output byte lengthof input DUP(?)
 	shift dword 3
 
 .code
 	main proc
 		
-		;mov esi, 0							; current index
-		;mov ebx, shift
-		;mov ecx, lengthof input				; counter == input[]
-		;L1:
-		;	mov al, input[esi]				; get a character from source
-		;	mov output[ebx], al				; store it in the target
-			
-		;	inc esi							; move to next character in output[]
-		;	inc ebx
-		;loop L1
-
 		mov eax, 0
 		mov ebx, 0
 		mov ebx, lengthof input				; length of input
@@ -40,6 +29,25 @@ ExitProcess proto,dwExitCode:dword
 			inc ebx							; move to next character in input[]
 		loop L2
 
+		mov esi, 0							; current index
+		mov ebx, shift
+		mov ecx, lengthof input				; counter == input[]
+		L1:
+			mov al, input[esi]				; get a character from source
+			mov output[ebx], al				; store it in the target
+			
+			inc esi							; move to next character in output[]
+			inc ebx
+		loop L1
+
+
+		mov esi, 0
+		mov ecx, 0
+		mov ecx, lengthof output
+		L3:
+			mov al, output[esi]
+			inc esi
+		loop L3
 
 		
 		invoke ExitProcess, 0
