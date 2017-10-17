4bbaf4a78fcaed85dd21b762a27a70d076579a86
Ash <a.searle274@gmail.com>
2017-10-16 20:32:12 -0500

    both sides of shift, need to fix bug

--- sch17f__comp3350__hw3/main.asm
+++ sch17f__comp3350__hw3/main.asm
@@ -8,34 +8,39 @@
 ExitProcess proto,dwExitCode:dword
 
 .data
-	input byte 4,06h,8,0Ch
+	input byte  5,0Ah,3,6,0Ch
 	output byte lengthof input DUP(?)
-	shift dword 2
+	shift dword 3
 
 .code
 	main proc
-		mov esi, 0							; current index
-		mov ebx, shift						; index + shift
-		mov ecx, lengthof input				; counter == input[]
-		L:
-			mov al, input[esi]				; get a character from source
-			mov output[ebx], al				; store it in the target
-			inc esi							; move to next character in input[]
-			inc ebx							; move to next character in output[]
-		loop L
 		
-		
-		
-		
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		; loop for testing
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		;mov esi, 0							; current index
+		;mov ebx, shift
+		;mov ecx, lengthof input				; counter == input[]
+		;L1:
+		;	mov al, input[esi]				; get a character from source
+		;	mov output[ebx], al				; store it in the target
+			
+		;	inc esi							; move to next character in output[]
+		;	inc ebx
+		;loop L1
+
+		mov eax, 0
+		mov ebx, 0
+		mov ebx, lengthof input				; length of input
+		sub ebx, shift						; length of input - shift
 		mov esi, 0
-		mov ecx, lengthof input
-		A:
-			mov al, output[esi]				; get a character from source
-			inc esi							; move to next character
-		loop A
+		mov ecx, shift
+		L2:
+			mov al, input[ebx]				; get first runoff character from source
+			mov output[esi], al				; store it in begining of target
+		
+			inc esi
+			inc ebx							; move to next character in input[]
+		loop L2
+
+
 		
 		invoke ExitProcess, 0
 	main endp
